7d56086c9b9f9b7f503f5fd93a33145a93ced243
Nir Zagdanski <zeged@yahoo.com>
2017-01-20 16:49:58 +0200

    rdy to rank single term?

--- WpfApplication2/Searcher.cs
+++ WpfApplication2/Searcher.cs
@@ -10,11 +10,7 @@ namespace IR_Engine
     public class Searcher : ISearcher
     {
 
-        SortedList<int, int>[] termResult;
-
-     //   public se
-
-
+    
         public event PropertyChangedEventHandler PropertyChanged;
 
         public void NotifyPropertyChanged(string PropName)
@@ -36,9 +32,9 @@ namespace IR_Engine
         /// MVVM
 
             
-            public string getDocData(int doc)
+            public Tuple<string,string,int,string,int,int,int,int> getDocData(int doc)
         {
-            string ans = string.Empty;
+            // string ans = string.Empty;
 
             if (Indexer.DocumentMetadata.ContainsKey(doc))
             {
@@ -54,170 +50,220 @@ namespace IR_Engine
                 int totalInDocwithoutSW = Int32.Parse(val[6]);
                 int AmountUniqueInCorpus = Int32.Parse(val[7]);
 
-          
+
                 ///12: FBIS3-521 , ifp : 5, English , uniqueInDoc : 74, totalInDocIncludingSW : 201, totalInDocwithoutSW : 114}@12 #Unique in corpus:3
                 ///13: FBIS3-85 , 2-28 : 2, French , uniqueInDoc : 66, totalInDocIncludingSW : 164, totalInDocwithoutSW : 89}@13 #Unique in corpus:3
                 ///14: FBIS3-52 , report : 32, , uniqueInDoc : 562, totalInDocIncludingSW : 1885, totalInDocwithoutSW : 1177}@14 #Unique in corpus:129
                 ///15: FBIS3-86 , oau : 6, Arabic , uniqueInDoc : 95, totalInDocIncludingSW : 264, totalInDocwithoutSW : 152}@15 #Unique in corpus:8
-                 ans= DOCNO + "| max_tf (in Doc):" + mostFreqTermInDoc + "| tf (in Doc):" + maxOccurencesInDocument +
-                  "| Language:" + language + "| UniqueInDoc :" + uniqueInDocAmount + "| totalInDocIncludingSW :" + totalInDocIncludingSW
-                +"| totalInDocwithoutSW:" + totalInDocwithoutSW + "| AmountUniqueInCorpus:" + AmountUniqueInCorpus;
+            ///    ans = DOCNO + "| max_tf (in Doc):" + mostFreqTermInDoc + "| tf (in Doc):" + maxOccurencesInDocument +
+            ///     "| Language:" + language + "| UniqueInDoc :" + uniqueInDocAmount + "| totalInDocIncludingSW :" + totalInDocIncludingSW
+            ///   + "| totalInDocwithoutSW:" + totalInDocwithoutSW + "| AmountUniqueInCorpus:" + AmountUniqueInCorpus;
 
-              
+
+                //}
+
+
+
+
+                //Tuple<int, string, bool> tuple =
+                return new Tuple<string, string, int, string, int, int, int, int>(DOCNO, mostFreqTermInDoc,
+                maxOccurencesInDocument, language, uniqueInDocAmount, totalInDocIncludingSW, totalInDocwithoutSW, AmountUniqueInCorpus);
             }
-            return ans;
+
+            return null;
         }
 
-        
 
-        public bool proccessQuery(string querySingleTerm)
+        public Tuple<SortedList<int, int>, int, int> getReleventDocumentsOfSingleTerm(string querySingleTerm)
         {
-
-            //http://www.c-sharpcorner.com/UploadFile/dpatra/autocomplete-textbox-in-wpf/
-            //foreach startswith
-
-         //   foreach (string s in )
-            
-
-
-
+            string val = Indexer.myDictionary[querySingleTerm];
             //#tf
             int termFrequency; // in all corpurus //single
             //#df
             int documentFrequenct;
 
-     
+
+
+            //count docs
+            char[] delimiterCharsLang = { '#' };
+            string[] termData = val.Split(delimiterCharsLang);
+
+            int.TryParse(termData[1], out termFrequency);
+            int.TryParse(termData[2], out documentFrequenct);
+
+
+
+
+            //MVVM
+            DocResult = "Term: " + '"' + querySingleTerm + '"' + " " + "tf:" + termFrequency + " df:" + documentFrequenct;
+            //MVVM
+
+
+            Console.WriteLine("Term: " + '"' + querySingleTerm + '"' + " " + "tf:" + termFrequency + " df:" + documentFrequenct);
+
+
+            /////////////////
+
+            int length = termData.Length;
+            int document;
+            int frequency;
+
+            //   termResult = new SortedList<int, int>[1];
+
+            //relervent documents. sorted by docNum, sort/rank by termFreq
+
+            //    termResult[0] = new SortedList<int, int>();
+
+            //TERM1, DOCS + #
+            //TERM1-TERM2, DOCS + #
+
+            ///popular -term
+            /// TERM1-TERM2  ^#3 freqInAllCorpus  #2 amountOfDocs | (DOC#:2 - 2 TIMES) (DOC#:4 - 1 TIMES)
+            /// 10-percen ^# 2 # 2 # 29#1 # 77#1
+            ///
+            /// 
+            ///10-percen+percent^#2#2#29#1#77#1
+            ///10-yea^#2#2#40#1#88#1
+            ///10-yea+year^#2#2#40#1#88#1
+            ///
+
+
+
+
+            ///return most popular doc
+            ///
+            /// sort/rank docs 1.mostPopularTerm+freq 2.amountOfUniqueInDoc by 3.SizeOfDoc w/wo SW
+            /// 
+            ///2$ FBIS3-2 , public : 95, , uniqueInDoc : 817, totalInDocIncludingSW : 4328, totalInDocwithoutSW : 3101}@2
+
+            //find all relevent documents
+
+            //rank docs by termFreqinDoc
+
+
+
+            SortedList<int, int> termResult = new SortedList<int, int>();
+
+
+
+            //PARSE DICTIONARY TERMPOST_TUPLE TO DOC#-FREQ
+            //FIRST TERM
+            for (int i = 3; i < length - 1; i = i + 2)
+            {
+                int.TryParse(termData[i], out document); //DOCUMENT
+                int.TryParse(termData[i + 1], out frequency); //FREQUENCY
+
+                termResult.Add(document, frequency); //ADD to LIST OF DOCS
+                                                     //doc
+                                                     //freq
+            }
+
+
+
+
+            //termresult
+            //return list of docs + termFrequency + documentFreq
+
+
+
+
+
+
+
+
+
+
+
+            ///language
+            ///
+            //retrieve final document
+
+            //use postingList??
+            //https://www.youtube.com/watch?v=IZTJgMjE5jw -- recommended
+
+
+
+
+            //return document list
+
+            return new Tuple<SortedList<int, int>,int,int> (termResult, termFrequency, documentFrequenct);
+
+        }
+
+        public void sortDocList (SortedList<int, int> termResult)
+        {
+
+            //GET ALL lISTS OF DOCS
+            //AND SORT THEM BY FREQ
+
+            //  termResult.sortby freq
+
+
+            ///http://stackoverflow.com/questions/28923062/sortedlistint-int-sort-by-value
+            ///var orderByVal = sortedlist.OrderBy(v => v.Value);
+            ///
+
+            var orderByVal = termResult.OrderBy(v => v.Value);
+
+            //reverse descending
+            //http://stackoverflow.com/questions/7815930/sortedlist-desc-order
+
+            orderByVal.Reverse();
+
+            var desc = orderByVal.Reverse();
+
+            //better just change comperer
+            //http://stackoverflow.com/questions/7815930/sortedlist-desc-order
+
+
+
+            DocResult += System.Environment.NewLine;
+            //display the elements of the sortedlist after sorting it with Value's
+            Console.WriteLine("The elements in the SortedList after sorting :");
+            foreach (KeyValuePair<int, int> pair in desc)
+            {
+
+                Console.WriteLine("DocNo:{0} => Popularity:{1}", pair.Key, pair.Value);
+
+                DocResult += "DocNo: {" + pair.Key + "} => Popularity: {" + pair.Value + "}" + getDocData(pair.Key);
+
+                DocResult += System.Environment.NewLine;
+
+            }
+
+        }
+
+
+        ///http://stackoverflow.com/questions/3663546/most-efficient-way-to-find-the-index-of-an-item-in-a-sorteddictionary
+        ///http://stackoverflow.com/questions/8090786/c-sharp-sorted-list-how-to-get-the-next-element
+        ///http://stackoverflow.com/questions/6131827/how-do-i-get-the-element-with-the-smallest-key-in-a-collection-in-o1-or-olog
+        ///CREATE SORTEDLIST
+        public bool proccessQuery(string querySingleTerm)
+        {
+
+            //http://www.c-sharpcorner.com/UploadFile/dpatra/autocomplete-textbox-in-wpf/
+          
+
+
+            //foreach term in full query
+
+
 
             if (Indexer.myDictionary  .ContainsKey(querySingleTerm)) //single term
             {
-             
-                string val = Indexer.myDictionary[querySingleTerm];
+                Tuple<SortedList<int, int>, int, int> termData = getReleventDocumentsOfSingleTerm(querySingleTerm);
 
-
-
-                //count docs
-                char[] delimiterCharsLang = { '#'};
-                string[] termData  = val.Split(delimiterCharsLang);
-
-                int.TryParse(termData[1], out termFrequency);
-                int.TryParse(termData[2], out documentFrequenct);
+                SortedList<int, int> termResult = termData.Item1;
+                int termFreq = termData.Item2;
+                int docFreq = termData.Item3;
 
 
 
 
-                //MVVM
-                DocResult = "Term: " + '"' + querySingleTerm + '"' + " " + "tf:" + termFrequency + " df:" + documentFrequenct;
-                //MVVM
 
 
-                Console.WriteLine("Term: " + '"' + querySingleTerm + '"' + " " + "tf:" + termFrequency + " df:" + documentFrequenct);
+                //send to ranker?
 
-
-                /////////////////
-
-                int length = termData.Length;
-                int document;
-                int frequency;
-
-                termResult = new SortedList<int, int>[1];
-
-                //relervent documents. sorted by docNum, sort/rank by termFreq
-
-                termResult[0] = new SortedList<int, int>();
-
-                //TERM1, DOCS + #
-                //TERM1-TERM2, DOCS + #
-
-                ///popular -term
-                /// TERM1-TERM2  ^#3 freqInAllCorpus  #2 amountOfDocs | (DOC#:2 - 2 TIMES) (DOC#:4 - 1 TIMES)
-                /// 10-percen ^# 2 # 2 # 29#1 # 77#1
-                ///
-                /// 
-                ///10-percen+percent^#2#2#29#1#77#1
-                ///10-yea^#2#2#40#1#88#1
-                ///10-yea+year^#2#2#40#1#88#1
-                ///
-
-
-
-
-                ///return most popular doc
-                ///
-                /// sort/rank docs 1.mostPopularTerm+freq 2.amountOfUniqueInDoc by 3.SizeOfDoc w/wo SW
-                /// 
-                ///2$ FBIS3-2 , public : 95, , uniqueInDoc : 817, totalInDocIncludingSW : 4328, totalInDocwithoutSW : 3101}@2
-
-                //find all relevent documents
-
-                //rank docs by termFreqinDoc
-                //return doc/s
-
-
-
-
-                //present docs on screen
-
-
-                //PARSE DICTIONARY TERMPOST_TUPLE TO DOC#-FREQ
-                //FIRST TERM
-                for (int i=3; i <length-1; i=i+2)
-                {
-                    int.TryParse(termData[i], out document); //DOCUMENT
-                    int.TryParse(termData[i+1], out frequency); //FREQUENCY
-
-                    termResult[0].Add(document, frequency); //ADD TO ARRAY OF LISTS OF DOCS
-                    //doc
-                    //freq
-                }
-
-                // string test = Indexer.myPostings[query].Skip(1).Take(1).ToDictionary;
-
-                //GET ALL lISTS OF DOCS
-                //AND SORT THEM BY FREQ
-
-              //  termResult.sortby freq
-
-
-                    ///http://stackoverflow.com/questions/28923062/sortedlistint-int-sort-by-value
-                    ///var orderByVal = sortedlist.OrderBy(v => v.Value);
-                    ///
-
-                var orderByVal = termResult[0].OrderBy(v => v.Value);
-
-                //reverse descending
-                //http://stackoverflow.com/questions/7815930/sortedlist-desc-order
-
-                orderByVal.Reverse();
-
-                var desc = orderByVal.Reverse();
-
-                //better just change comperer
-                //http://stackoverflow.com/questions/7815930/sortedlist-desc-order
-
-
-
-                DocResult += System.Environment.NewLine;
-                //display the elements of the sortedlist after sorting it with Value's
-                Console.WriteLine("The elements in the SortedList after sorting :");
-                foreach (KeyValuePair<int, int> pair in desc)
-                {
-
-                    Console.WriteLine("DocNo:{0} => Popularity:{1}", pair.Key, pair.Value);
-
-                    DocResult += "DocNo: {" +  pair.Key + "} => Popularity: {" +pair.Value + "}" + getDocData(pair.Key);
-
-                    DocResult += System.Environment.NewLine;
-
-                }
-
-
-
-                ///language
-                ///
-                //retrieve final document
-
-                //use postingList??
-                //https://www.youtube.com/watch?v=IZTJgMjE5jw -- recommended
 
 
 
@@ -228,157 +274,6 @@ namespace IR_Engine
 
                 //show most 1.popularByFreq 2.Original Term/Additional -Term2
 
-
-
-                ///http://stackoverflow.com/questions/3663546/most-efficient-way-to-find-the-index-of-an-item-in-a-sorteddictionary
-                ///http://stackoverflow.com/questions/8090786/c-sharp-sorted-list-how-to-get-the-next-element
-                ///http://stackoverflow.com/questions/6131827/how-do-i-get-the-element-with-the-smallest-key-in-a-collection-in-o1-or-olog
-                ///CREATE SORTEDLIST
-                ///
-
-                //     Indexer.myPostings.
-
-                // SortedList<string, string> sdfg = new SortedList<string, string>();
-
-                //   var s = new SortedList<string, string>{ { "a", "Ay" }, { "b", "Bee" }, { "c", "Cee" } };
-
-
-                // Outputs 1
-                //    Console.WriteLine(s.IndexOfKey("b"));
-                //   if (Indexer.myPostings.ContainsKey(query))
-
-                //   var index = Indexer.myDictionary.IndexOfKey(key);
-
-                //   var first = Indexer.myDictionary.Values[index];
-                //   var second = Indexer.myDictionary.Values[index + 1];
-
-                //  SortedList can be accessed by both key and index
-
-                //   var IndexOfKey = Indexer.myDictionary.IndexOfKey(key);
-                //Increment the index,
-
-
-
-
-
-
-
-
-
-                //string val;
-
-                //query
-
-                //GET NEXT ELEMT
-                //QUERY TERM-TERM
-
-                //GET INDEX-KEY OF TERM IN DATABASE
-                var IndexOfKey = Indexer.myDictionary.IndexOfKey(querySingleTerm);
-
-
-                //GET NEXT TERM
-                // TERM1-TERM2
-
-                IndexOfKey++; //Handle last index case
-                              //Get the next item by index.
-
-                char[] delimiterDocs = { '+', '-' };
-
-                string nextTermValue = Indexer.myDictionary.Values[IndexOfKey];
-                string nextTermFullKey = Indexer.myDictionary.Keys[IndexOfKey];
-
-                string nextTerm1stKey = nextTermFullKey.Split(delimiterDocs)[0];
-
-                Console.WriteLine(nextTermFullKey + ":" + nextTermValue);
-
-
-                // querySingleTerm.Substring(querySingleTerm.Length-1) = "-related"
-
-
-            //    string related;
-                //        related = 
-
-                // querySingleTerm.Substring(0,querySingleTerm.Length-1) = "term1"
-
-
-
-                //or querySingleTerm.split('-')[] // all related
-
-                //if [0] === term
-
-                //while [0] == term
-            
-              //  string nextTerm1stKey = nextTermFullKey.Split(delimiterDocs)[0];
-
-
-                while (nextTerm1stKey == querySingleTerm)//while term = "term1-"
-                {
-
-                    IndexOfKey++; //Handle last index case
-                                  //Get the next item by index.
-
-
-
-
-
-
-
-                    nextTermValue = Indexer.myDictionary.Values[IndexOfKey];
-                    nextTermFullKey = Indexer.myDictionary.Keys[IndexOfKey];
-                    nextTerm1stKey = nextTermFullKey.Split(delimiterDocs)[0];
-
-
-
-                    ////
-
-
-
-                //    function above
-
-
-
-                    ////
-                    Console.WriteLine(nextTermFullKey + ":" + nextTermValue);
-
-              
-
-
-                }
-
-          //      char[] delimiterDocs = { '+', '-'  };
-          //      string nextTerm1stKey = nextTermFullKey.Split(delimiterDocs)[0];
-
-          
-                //need to get alll term-term
-                //find most popular
-
-                //this returns only documents of the 1st term in query
-                //all term-
-
-
-                //if input = term1-term2
-                //search term1-
-                //search term2-
-                //search term1-term2
-                //search term2-term1
-
-                //day-by-day
-
-
-                ///output all documents to ranker?
-                ///vector search
-
-                //need two more terms
-
-                //intersect relevence of long-term in all documents
-
-
-
-
-                //return most relevent document
-
-                //language
-
                 return true;
             }
             else
@@ -388,6 +283,78 @@ namespace IR_Engine
             return false;
 
         }
+
+
+        /// <summary>
+        /// retun list of querySingleTerm-term 
+        /// </summary>
+        /// <param name="querySingleTerm"></param>
+        public void autoComplete(string querySingleTerm)
+        {
+
+            //    //AUTO COMPLETE
+
+            //GET NEXT ELEMT
+            //QUERY TERM-TERM
+
+            //GET INDEX-KEY OF TERM IN DATABASE
+            var IndexOfKey = Indexer.myDictionary.IndexOfKey(querySingleTerm);
+
+
+            //GET NEXT TERM
+            // TERM1-TERM2
+
+            IndexOfKey++; //Handle last index case
+                          //Get the next item by index.
+
+            char[] delimiterDocs = { '+', '-' };
+
+            string nextTermValue = Indexer.myDictionary.Values[IndexOfKey];
+            string nextTermFullKey = Indexer.myDictionary.Keys[IndexOfKey];
+
+            string nextTerm1stKey = nextTermFullKey.Split(delimiterDocs)[0];
+
+            Console.WriteLine(nextTermFullKey + ":" + nextTermValue);
+
+
+
+
+
+            while (nextTerm1stKey == querySingleTerm)//while term = "term1-"
+            {
+
+                IndexOfKey++; //Handle last index case
+                              //Get the next item by index.
+
+
+
+
+
+
+
+                nextTermValue = Indexer.myDictionary.Values[IndexOfKey];
+                nextTermFullKey = Indexer.myDictionary.Keys[IndexOfKey];
+                nextTerm1stKey = nextTermFullKey.Split(delimiterDocs)[0];
+
+
+
+                ////
+
+
+
+                //    function above
+
+
+
+                ////
+                Console.WriteLine(nextTermFullKey + ":" + nextTermValue);
+
+
+
+
+            }
+        }
+
 
         public void move(double speed, int angle)
         {
